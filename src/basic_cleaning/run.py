b8b94a2c78d514448d82b27bda1dcc695dfdd162
nosborne11 <nosborne11@gmail.com>
2021-12-20 18:09:58 +0200

    Updated with dropping improper geolocations

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    #Drop rows not in proper geolocations
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
 
     logger.info("Saving Data and Logging Artifact")
 
